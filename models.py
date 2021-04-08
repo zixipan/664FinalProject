a0e8d4e6a0a66045202dbd2c85a7111e83617654
zixipan <54960538+zixipan@users.noreply.github.com>
2021-04-09 02:27:17 +0800

    Update models.py

--- models.py
+++ models.py
@@ -10,6 +10,26 @@ class Kind(models.Model):
     )
     breed = models.TextField()
 
+class Contact(models.Model):
+    email = models.CharField(
+            max_length=50,
+            validators=[MinLengthValidator(2, "Email must be greater than 2 characters")]
+    )
+    phone = models.DecimalField(max_digits=15, decimal_places=0, null=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Location(models.Model):
+    state = models.CharField(
+            max_length=2,
+            validators=[MinLengthValidator(1, "State must be greater than 1 character")]
+    )
+    zipcode = models.TextField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
 
 class Pet(models.Model) :
     title = models.CharField(
@@ -22,11 +42,9 @@ class Pet(models.Model) :
     comments = models.ManyToManyField(settings.AUTH_USER_MODEL,
         through='Comment', related_name='comments_owned')
 
-    contacts = models.ManyToManyField(settings.AUTH_USER_MODEL,
-        through='Contact', related_name='contacts_owned')
-
-    locations = models.ManyToManyField(settings.AUTH_USER_MODEL,
-        through='Location', related_name='locations_owned')
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    kind = models.ForeignKey(Kind, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
     # Picture
     picture = models.BinaryField(null=True, editable=True)
     content_type = models.CharField(max_length=256, null=True, help_text='The MIMEType of the file')
@@ -37,8 +55,6 @@ class Pet(models.Model) :
     # Favorites
     favorites = models.ManyToManyField(settings.AUTH_USER_MODEL,
         through='Fav', related_name='favorite_pets')
-
-    kind = models.ForeignKey(Kind, on_delete=models.CASCADE)
 
     # Shows up in the admin list
     def __str__(self):
@@ -59,29 +75,6 @@ class Comment(models.Model) :
     def __str__(self):
         if len(self.text) < 15 : return self.text
         return self.text[:11] + ' ...'
-
-
-class Contact(models.Model):
-    email = models.CharField(
-            max_length=50,
-            validators=[MinLengthValidator(2, "Email must be greater than 2 characters")]
-    )
-    phone = models.DecimalField(max_digits=15, decimal_places=0, null=True)
-    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-class Location(models.Model):
-    state = models.CharField(
-            max_length=2,
-            validators=[MinLengthValidator(1, "State must be greater than 1 character")]
-    )
-    zipcode = models.TextField()
-    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 
 class Fav(models.Model) :
